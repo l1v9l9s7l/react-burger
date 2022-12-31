edf7de267ee2af1acfd6d2d16067902410260337
l1v9l9s7l <l1v9l9s7l@gmail.com>
2022-12-31 05:37:53 +0300

    check

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,45 @@
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux/es/exports';
-import styles from './App.module.css';
-import AppHeader from '../AppHeader/AppHeader.jsx'
-import BurgerIngridients from '../BurgerIngridients/BurgerIngridients.jsx'
-import BurgerConstructor from '../BurgerConstructor/BurgerConstructor.jsx'
-import { getIngridients} from '../../services/actions/ingridientsAction';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import styles from "./App.module.css";
+import AppHeader from "../AppHeader/AppHeader.jsx";
+import BurgerIngridients from "../BurgerIngridients/BurgerIngridients.jsx";
+import BurgerConstructor from "../BurgerConstructor/BurgerConstructor.jsx";
+import { getIngridients } from "../../services/actions/ingridientsAction";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { Login } from "../../pages/Login/Login";
+import { Register } from "../../pages/Register/Register";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  useEffect(()=> {
-    dispatch(getIngridients())
-  }, [dispatch])
-
+  useEffect(() => {
+    dispatch(getIngridients());
+  }, [dispatch]);
 
   return (
     <>
-      <DndProvider backend={HTML5Backend}>
-        <AppHeader />
-        <main className={styles.main}>
-          <BurgerIngridients />
-          <BurgerConstructor  />
-        </main>
-        </DndProvider>
+      <AppHeader />
+      <main className={styles.main}>
+        <Switch>
+          <Route path="/" exact>
+            <DndProvider backend={HTML5Backend}>
+              <BurgerIngridients />
+              <BurgerConstructor />
+            </DndProvider>
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </main>
     </>
   );
 }
 
 export default App;
-
-
--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,10 +1,25 @@
-import styles from './AppHeader.module.css'
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
+import styles from "./AppHeader.module.css";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
 import {
-  BurgerIcon, CloseIcon, CheckMarkIcon, CurrencyIcon, DragIcon, EditIcon, HideIcon, InfoIcon, ListIcon, LockIcon, LogoutIcon, ProfileIcon, ShowIcon,
-  DeleteIcon, ArrowUpIcon, ArrowDownIcon, MenuIcon
-} from '@ya.praktikum/react-developer-burger-ui-components';
-
+  BurgerIcon,
+  CloseIcon,
+  CheckMarkIcon,
+  CurrencyIcon,
+  DragIcon,
+  EditIcon,
+  HideIcon,
+  InfoIcon,
+  ListIcon,
+  LockIcon,
+  LogoutIcon,
+  ProfileIcon,
+  ShowIcon,
+  DeleteIcon,
+  ArrowUpIcon,
+  ArrowDownIcon,
+  MenuIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link } from "react-router-dom";
 
 export default function AppHeader() {
   return (
@@ -12,26 +27,25 @@ export default function AppHeader() {
       <div className={styles.content}>
         <div className={styles.navigation}>
           <a className={styles.headerConstructor}>
-            <div className='pl-5'></div>
+            <div className="pl-5"></div>
             <BurgerIcon />
-            <p className='pl-2 pr-5'>Конструктор</p>
+            <p className="pl-2 pr-5">Конструктор</p>
           </a>
           <a className={`${styles.headerOrderList} pl-2`}>
             <div className={`${styles.navigationIcon} pl-5`}>
-              <ListIcon type='secondary' />
+              <ListIcon type="secondary" />
             </div>
-            <p className='text_color_inactive pl-2 pr-5'>Лента заказов</p>
+            <p className="text_color_inactive pl-2 pr-5">Лента заказов</p>
           </a>
         </div>
-        <div className={styles.logo}>
+        <Link to="/" className={styles.logo}>
           <Logo />
-        </div>
-        <a href="#" className={styles.profile}>
-          <ProfileIcon type='secondary' />
-          <p className={`${styles.profileText} ${styles.text_color_inactive}`} >Личный кабинет</p>
-        </a>
+        </Link>
+        <Link className={styles.profile} to="/login">
+          <ProfileIcon type="secondary" />
+          <p className={`${styles.profileText} ${styles.text_color_inactive}`}>Личный кабинет</p>
+        </Link>
       </div>
-    </header >
-  )
+    </header>
+  );
 }
-
